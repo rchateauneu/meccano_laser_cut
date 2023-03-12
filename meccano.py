22b2bc0be8f8523b554eb64b9a5a77a1b26b35be
Primhill Computers <remi.chateauneu@primhillcomputers.com>
2023-03-12 22:56:41 +0000

    Redesign.

--- meccano.py
+++ meccano.py
@@ -16,133 +16,201 @@ HOLES_RIGHT = 2
 HOLES_UP = 3
 HOLES_DOWN = 4
 
-class MeccanoPart:
-    def __init__(self, x_size, y_size):
-        self._holes = set()
-        self._x_size = x_size
-        self._y_size = y_size
-        
-    def _AddHoles(self, x_pos, y_pos):
-        if x_pos < 0:
-            raise Exception("Negative x_pos ", x_pos)
-        if x_pos >= self._x_size:
-            raise Exception("Too big x_pos ", x_pos)
-        if y_pos < 0:
-            raise Exception("Negative y_pos ", y_pos)
-        if y_pos >= self._y_size:
-            raise Exception("Too big y_pos ", y_pos)
-        self._holes.add((x_pos, y_pos))
-        #print("Adding", (x_pos, y_pos))
+# TODO : Create a part from a bitmap where each black pixel indicates a hole.
 
-    def Line(self, x_pos, y_pos, number, direction = HOLES_RIGHT):
-        print("x_pos=", x_pos, "y_pos=", y_pos, "number=", number)
-        for index in range(number):
-            self._AddHoles(x_pos, y_pos)
-            if direction == HOLES_RIGHT:
-                x_pos += 1
-            elif direction == HOLES_LEFT:
-                x_pos -= 1
-            elif direction == HOLES_DOWN:
-                y_pos += 1
-            elif direction == HOLES_UP:
-                y_pos -= 1
-            else:
-                raise Exception("Invalid direction", direction)
-                
-    def Rectangle(self, x_pos, y_pos, x_number, y_number):
-        print("x_pos=", x_pos, "y_pos=", y_pos, "x_number=", x_number, "y_number=", y_number)
-        for index in range(x_number):
-            self.Line(x_pos, y_pos, y_number, HOLES_DOWN)
-            x_pos += 1
-            
-    # Triplet pythagoricien.
-    # 3 - 4 - 5
-    # 5 - 12 - 13
-    # https://fr.wikipedia.org/wiki/Triplet_pythagoricien
-    # Find the closest Pythagorean triple.
-    # This creates a right triangle.
-    # This is useful to assemble sheets on non-right angles.
-    # They could be used to triangularize 3D spline curves.
-    def Pythagore(self, x_pos, y_pos, x_number, y_number):
-        x_iter = x_number
-        y_iter = y_number
-        # TODO: This is not a very good function.
-        while True:
-            print("Triangle : Trying x=", x_iter, "y=", y_iter)
-            c_square = x_iter * x_iter + y_iter * y_iter
-            c_number = int(math.sqrt(c_square))
-            if c_number * c_number == c_square:
-                break
-            if (x_iter - x_number) / x_number > (y_iter - y_number) / y_number:
-                y_iter += 1
-            else:
-                x_iter += 1
-        print("Triangle : OK x=", x_iter, "y=", y_iter, "x=", c_number)
-        self.Line(x_pos, y_pos, x_iter + 1, HOLES_RIGHT)
-        self.Line(x_pos, y_pos, y_iter + 1, HOLES_DOWN)
-        ratio = 1.0 / (c_number + 1.0)
-        for c_index in range(1, c_number + 1):
-            x_hole = x_pos + x_iter * c_index * ratio
-            y_hole = y_pos + y_iter * (1.0 - c_index * ratio)
-            self._holes.add((x_hole, y_hole))
-            
+
+def _DrawBorderSquare(the_output, x_size, y_size):
+    x_end = x_size * mecano_step
+    y_end = y_size * mecano_step
+
+    draw_line_actual(the_output, 0, 0, 0, y_end, COLOR_BLACK)
+    draw_line_actual(the_output, 0, y_end, x_end, y_end, COLOR_BLACK)
+    draw_line_actual(the_output, x_end, y_end, x_end, 0, COLOR_BLACK)
+    draw_line_actual(the_output, x_end, 0, 0, 0, COLOR_BLACK)
+
+def _DrawBorderRounded(the_output, x_size, y_size):
+    half_step = mecano_step * 0.5
+    x_end = x_size * mecano_step
+    y_end = y_size * mecano_step
+    x_end_half = x_end - half_step
+    y_end_half = y_end - half_step
+
+    draw_line_actual(the_output, 0, half_step, 0, y_end_half, COLOR_BLACK)
+    draw_line_actual(the_output, half_step, y_end, x_end_half, y_end, COLOR_BLACK)
+    draw_line_actual(the_output, x_end, y_end_half, x_end, half_step, COLOR_BLACK)
+    draw_line_actual(the_output, x_end_half, 0, half_step, 0, COLOR_BLACK)
+
+    right_angle = 90.0
+
+    border_radius = mecano_step * 0.5
+    draw_arc(the_output, half_step, half_step, border_radius, 2 * right_angle, 3 * right_angle, COLOR_BLACK)
+    draw_arc(the_output, half_step, y_end_half, border_radius, right_angle, 2 * right_angle, COLOR_BLACK)
+    draw_arc(the_output, x_end_half, y_end_half, border_radius, 0, right_angle, COLOR_BLACK)
+    draw_arc(the_output, x_end_half, half_step, border_radius, 3 * right_angle, 0.0, COLOR_BLACK)
+
+def _DrawBorder(the_output, x_size, y_size):
     # TODO: Ajouter un flag pour des bords droits ou arrondis.
-    def _DrawBorderSquare(self, the_output):
-        x_end = self._x_size * mecano_step
-        y_end = self._y_size * mecano_step
+    _DrawBorderRounded(the_output, x_size, y_size)
 
-        draw_line_actual(the_output, 0, 0, 0, y_end, COLOR_BLACK)
-        draw_line_actual(the_output, 0, y_end, x_end, y_end, COLOR_BLACK)
-        draw_line_actual(the_output, x_end, y_end, x_end, 0, COLOR_BLACK)
-        draw_line_actual(the_output, x_end, 0, 0, 0, COLOR_BLACK)
 
-    def _DrawBorderRounded(self, the_output):
-        half_step = mecano_step * 0.5
-        x_end = self._x_size * mecano_step
-        y_end = self._y_size * mecano_step
-        x_end_half = x_end - half_step
-        y_end_half = y_end - half_step
+def _DrawBorderTriangle(the_output, x_size, y_size):
+    half_step = mecano_step * 0.5
+    x_end = x_size * mecano_step
+    y_end = y_size * mecano_step
+    x_end_half = x_end - half_step
+    y_end_half = y_end - half_step
+    angle = math.atan(y_size / x_size) * 180.0 / 3.14159
 
-        draw_line_actual(the_output, 0, half_step, 0, y_end_half, COLOR_BLACK)
-        draw_line_actual(the_output, half_step, y_end, x_end_half, y_end, COLOR_BLACK)
-        draw_line_actual(the_output, x_end, y_end_half, x_end, half_step, COLOR_BLACK)
-        draw_line_actual(the_output, x_end_half, 0, half_step, 0, COLOR_BLACK)
+    draw_line_actual(the_output, 0, half_step, 0, y_end_half, COLOR_BLACK)
+    draw_line_actual(the_output, x_end_half, 0, half_step, 0, COLOR_BLACK)
+    draw_line_actual(the_output, x_end, 0, 0, y_end, COLOR_BLACK)
 
-        right_angle = 90.0
+    right_angle = 90.0
 
-        border_radius = mecano_step * 0.5
-        draw_arc(the_output, half_step, half_step, border_radius, 2 * right_angle, 3 * right_angle, COLOR_BLACK)
-        draw_arc(the_output, half_step, y_end_half, border_radius, right_angle, 2 * right_angle, COLOR_BLACK)
-        draw_arc(the_output, x_end_half, y_end_half, border_radius, 0, right_angle, COLOR_BLACK)
-        draw_arc(the_output, x_end_half, half_step, border_radius, 3 * right_angle, 0.0, COLOR_BLACK)
+    border_radius = mecano_step * 0.5
+    draw_arc(the_output, half_step, half_step, border_radius, 2 * right_angle, 3 * right_angle, COLOR_BLACK)
 
-    def DrawDxf(self, the_output):
-        init_output_fd(the_output)
-        self._DrawBorderRounded(the_output)
+    angle_top = right_angle - angle
+    draw_arc(the_output, half_step, y_end_half, border_radius, angle_top, 2 * right_angle, COLOR_BLACK)
+    angle_right = right_angle - angle
+    draw_arc(the_output, x_end_half, half_step, border_radius, 3 * right_angle, angle_right, COLOR_BLACK)
+
+
+class MeccanoPart:
+    def __init__(self, file_output):
+        self._holes = set()
+        self._file_output = file_output
+        init_output_fd(self._file_output)
+        
+    def Exit(self):
+        exit_output_fd(self._file_output)
+        
+    def DrawDxf(self):
+        #self._DrawBorderRounded(the_output, x_size, y_size)
         print("Number holes:", len(self._holes))
         for x_pos, y_pos in self._holes:
-            draw_hole_actual(the_output, (0.5 + x_pos) * mecano_step, (0.5 + y_pos) * mecano_step, mecano_hole, COLOR_BLACK)
-        exit_output_fd(the_output)
+            draw_hole_actual(self._file_output, (0.5 + x_pos) * mecano_step, (0.5 + y_pos) * mecano_step, mecano_hole, COLOR_BLACK)
+        
+
+    class ObjectTracer:
+        def __init__(self, meccano, x_org, y_org):
+            # So the origin is assumed to be zero.
+            self._meccano = meccano
+            print("x_org=", x_org, "y_org=", y_org)
+            self._x_org = x_org
+            self._y_org = y_org
+
+        def _AddHoles(self, x_pos, y_pos):
+            if x_pos < 0:
+                raise Exception("Negative x_pos ", x_pos)
+            if y_pos < 0:
+                raise Exception("Negative y_pos ", y_pos)
+            self._meccano._holes.add((self._x_org + x_pos, self._y_org + y_pos))
+
+        def Line(self, number, direction = HOLES_RIGHT):
+            x_pos, y_pos = 0, 0
+            for index in range(number):
+                self._AddHoles(x_pos, y_pos)
+                if direction == HOLES_RIGHT:
+                    x_pos += 1
+                elif direction == HOLES_LEFT:
+                    x_pos -= 1
+                elif direction == HOLES_DOWN:
+                    y_pos += 1
+                elif direction == HOLES_UP:
+                    y_pos -= 1
+                else:
+                    raise Exception("Invalid direction", direction)
+
+            def LineBoundary(the_output):
+                if direction in (HOLES_RIGHT, HOLES_LEFT):
+                    _DrawBorder(the_output, number, 1)
+                elif direction in (HOLES_DOWN, HOLES_UP):
+                    _DrawBorder(the_output, 1, number)
+                else:
+                    raise Exception("Invalid direction", direction)
+            return LineBoundary
+                    
+        def Rectangle(self, x_number, y_number):
+            print("x_number=", x_number, "y_number=", y_number)
+            x_pos = 0
+            for index in range(x_number):
+                y_pos = 0
+                for index in range(y_number):
+                    self._AddHoles(x_pos, y_pos)
+                    y_pos += 1
+                x_pos += 1
+                
+            def RectangleBoundary(the_output):
+                _DrawBorder(the_output, x_number, y_number)
+            return RectangleBoundary
+                
+        # Triplet pythagoricien.
+        # 3 - 4 - 5
+        # 5 - 12 - 13
+        # https://fr.wikipedia.org/wiki/Triplet_pythagoricien
+        # Find the closest Pythagorean triple.
+        # This creates a right triangle.
+        # This is useful to assemble sheets on non-right angles.
+        # They could be used to triangularize 3D spline curves.
+        def Pythagore(self, x_number, y_number):
+            x_iter = x_number
+            y_iter = y_number
+            # TODO: This is not a very good function.
+            while True:
+                print("Triangle : Trying x=", x_iter, "y=", y_iter)
+                c_square = x_iter * x_iter + y_iter * y_iter
+                c_number = int(math.sqrt(c_square))
+                if c_number * c_number == c_square:
+                    break
+                if (x_iter - x_number) / x_number > (y_iter - y_number) / y_number:
+                    y_iter += 1
+                else:
+                    x_iter += 1
+            print("Triangle : OK x=", x_iter, "y=", y_iter, "x=", c_number)
+
+            for x_pos_sub in range(x_number):
+                self._AddHoles(x_pos_sub, 0)
+            for y_pos_sub in range(y_number):
+                self._AddHoles(0, y_pos_sub)
+
+            ratio = 1.0 / (c_number + 1.0)
+            for c_index in range(1, c_number + 1):
+                x_hole = x_iter * c_index * ratio
+                y_hole = y_iter * (1.0 - c_index * ratio)
+                self._AddHoles(_hole, y_hole)
+            return x_iter, y_iter
+                
+            def TriangleBoundary(the_output):
+                _DrawBorder(the_output, x_number, y_number)
+            return RectangleBoundary
 
 
 if __name__ == "__main__":
-    # python .\meccano.py t_line.dxf 10 1 Line 0 0 10
-    # python .\meccano.py t_rect.dxf 10 5 Rectangle 0 0 10 5
-    filename = sys.argv[1]
+    # python .\meccano.py t_line.dxf Line 10
+    # python .\meccano.py t_rect.dxf Rectangle 10 5
+    # python .\meccano.py t_rect.dxf Triangle 10 5
+    
+    function_name = sys.argv[1]
+    print("function_name=", function_name)
+    
+    str_line_arguments = sys.argv[2:]
+    filename_end = "_".join(str_line_arguments)
+    filename = function_name + "_" + filename_end + ".dxf"
     print("filename=", filename)
-    if not filename.endswith(".dxf"):
-        raise Exception("Invalid filename", filename)
+    file_output = open(filename, "w");
 
-    x_size = int(sys.argv[2])
-    y_size = int(sys.argv[3])
-    meccano = MeccanoPart(x_size, y_size)
-    functionname = sys.argv[4]
-    print("functionname=", functionname)
-    function = getattr(meccano, functionname)
-    #exit()
-    fd = open(filename, "w");
+    int_line_arguments = [int(a) for a in str_line_arguments]
+
+    meccano = MeccanoPart(file_output)
+    # Position (0, 0)
+    tracer = MeccanoPart.ObjectTracer(meccano, 0, 0)
+
+    # By convention, all functions return a function which draws the boundary.
+    function_obj = getattr(tracer, function_name)
+    boundary_tracer = function_obj(*int_line_arguments)
+    boundary_tracer(file_output)
     
-    function( *[int(a) for a in sys.argv[5:]] )
-    
-    meccano.DrawDxf(fd)
-    fd.close()
+    meccano.DrawDxf()
+    meccano.Exit()
+    file_output.close()
